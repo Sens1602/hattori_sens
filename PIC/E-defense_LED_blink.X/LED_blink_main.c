178f3a729cf580785c9a301b0db13eef02d482f6
kanopero <p-hat69@fuji.waseda.jp>
2018-11-14 18:11:12 +0900

    steps

--- PIC/E-defense_LED_blink.X/LED_blink_main.c
+++ PIC/E-defense_LED_blink.X/LED_blink_main.c
@@ -42,34 +42,13 @@
 
 /**global variable************/
 #pragma udata
-char USB_Out_Buf[CDC_DATA_OUT_EP_SIZE];
-char USB_In_Buf[CDC_DATA_IN_EP_SIZE];
-USB_HANDLE    lastTransmission;
-unsigned char i;
-
-int err;//I2C??????????
+int i;
 int led_blightness = 0;
 int  blight_flag = 0;
-char outbuf[30];//
-unsigned long counter;
-int b;
-char temp;
-
-unsigned int accel_h_x;
-unsigned int accel_l_x;
-char bt_accel_h_x[3];
-char bt_accel_l_x[3];
-char *ptr_accel_h_x;
-char *ptr_accel_l_x;
 
 /**function prototype *******************/
 void YourHighPriorityISRCode();
 void YourLowPriorityISRCode();
-unsigned int WI2C( unsigned char control, 
-            unsigned char address, unsigned char data);
-unsigned int RI2C(unsigned char control, unsigned char address);
-void Calc();
-
 
 /*** interrupt vector***/
 #pragma code REMAPPED_HIGH_INTERRUPT_VECTOR = 0x08
@@ -85,42 +64,7 @@ void Remapped_Low_ISR (void){
 /****interrupt function***/
 #pragma interrupt YourHighPriorityISRCode
 void YourHighPriorityISRCode(){
-    if(INTCONbits.TMR0IF){  
-        INTCONbits.TMR0IF = 0; 
-        WriteTimer0(35416);
-
-        if(USB_BUS_SENSE && (USBGetDeviceState() == DETACHED_STATE))
-            USBDeviceAttach();
-
-        if((USBDeviceState >= CONFIGURED_STATE)&&(USBSuspendControl!=1)){
-            PORTBbits.RB5 ^= 1;
-            if(blight_flag ==0 && led_blightness<50){
-                led_blightness++;
-            }else if(led_blightness > 0){
-                blight_flag = 1;
-                led_blightness--;
-            }else{
-                blight_flag = 0;
-            }
-  
-            /*          
-            if(led_blightness != 40){
-                led_blightness = 40;
-            }else{
-                led_blightness = 0;
-            }                                                        
-            */
-            outbuf[0] = WI2C(0b11001010, 0x06, 100);
-            outbuf[1] = led_blightness;
-            
-            while(!USBUSARTIsTxTrfReady()) CDCTxService();//magic spell
-            putUSBUSART(outbuf, 2);
-            CDCTxService();
-        }      
-    }
-    #if defined(USB_INTERRUPT)
         USBDeviceTasks();
-    #endif
 }
 #pragma interruptlow YourLowPriorityISRCode
 void YourLowPriorityISRCode()
@@ -138,209 +82,11 @@ void main(void){
     TRISB = 0b00011100;
     TRISC = 0b00000000;
     USBDeviceInit();
-    
-    //I2C configration
-    //SSPADD = ((Fosc/4) / Fscl) - 1
-    OpenI2C(MASTER, SLEW_ON);   // master mode
-    SSPADD = 29;                //I2C400kHz?
-    err = WI2C(0b11001010, 0x01, 0b00000011);
-    err = WI2C(0b11001010, 0x06, 0x11111111);
-
-    //Timei0 configration
-    T0CON = 0b10000100;
-    WriteTimer0(35416);
-    INTCONbits.GIE = 1;
-    INTCONbits.TMR0IE = 1;
-        
+    PORTBbits.RB5 = 1;
     while(1){  
         if(USB_BUS_SENSE && (USBGetDeviceState() == DETACHED_STATE))
             USBDeviceAttach();	
     }
-}
-
-//I2C??????????
-unsigned int WI2C( unsigned char control, 
-            unsigned char address, unsigned char data){
-    IdleI2C();                                 // ??????
-    StartI2C();                                // start ???????
-   // if ( PIR2bits.BCLIF )return ( 1 );        // ??????? 
-// ---------------- ???????? -----------------------------
-    if(WriteI2C(control))return(2);           // ??????? 
-    IdleI2C();                                 // ??????
-    if (SSPCON2bits.ACKSTAT )return(3);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- MPU9250?????? ------------------------
-    if(WriteI2C(address))return(4);      // ????? 
-    IdleI2C();                                 // ??????
-    if ( SSPCON2bits.ACKSTAT )return(5);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- ???????? --------------------------
-    if(WriteI2C(data))return(8);              // ????????
-    IdleI2C();                                 // ??????  
-    if ( SSPCON2bits.ACKSTAT)return(9);       // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- Stop condition?? --------------------------
-    StopI2C();                                 // ???????
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-   // if ( PIR2bits.BCLIF )return ( 0 );       // ?????? 
-    return (0x64); 
-    
-}
-
-//I2C??????????
-unsigned int RI2C(unsigned char control, unsigned char address){
-    
-    IdleI2C();                                 // ??????
-    StartI2C();                                // start ???????
-   // if ( PIR2bits.BCLIF )return ( 8 );        // ??????? 
-        
-// ---------------- ???????? -----------------------------
-    if(WriteI2C(control))return(33);           // ??????? 
-    IdleI2C();                                 // ??????
-    if ( counter = SSPCON2bits.ACKSTAT )return(34);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- MPU9250?????? ------------------------
-    if(WriteI2C(address))return(35);      // ????? 
-    IdleI2C();                                 // ??????
-    if ( SSPCON2bits.ACKSTAT )return(36);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- Restart?? -----------------------
-    RestartI2C();                              // restart ????????????????????????
-    //if ( PIR2bits.BCLIF )return (37);        // ??????? 
-// ---------------- ??????Read Mode?? -----------------------------
-    if(WriteI2C(control+1))return(38);         // ??????? 
-    IdleI2C();// ??????
-    if ( SSPCON2bits.ACKSTAT )return(39);     // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- ?????? -----------------------------
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    NotAckI2C();                               // ACK?????Not ACK)
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- Stop condition?? --------------------------
-    StopI2C();                                 // ???????
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-    //if ( PIR2bits.BCLIF )return (40);       // ?????? 
-    PIR1bits.SSPIF = 0;                    // SSPIF???
-    return ( (unsigned int) SSPBUF );      // ????
-}
-
-//?????
-void Calc(){
-//----------------???,????-----------------
-    IdleI2C();                                 // ??????
-    StartI2C();                                // start ???????
-    //if ( PIR2bits.BCLIF )return ( 8 );       // ??????? 
-        
-// ---------------- ???????? -----------------------------
-    if(WriteI2C(0b11010000))return(33);        // ??????? 
-    IdleI2C();                                 // ??????
-    if ( counter = SSPCON2bits.ACKSTAT )return(34);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- MPU9250?????? ------------------------
-    if(WriteI2C(0x3B))return(35);      // ????? 
-    IdleI2C();                                 // ??????
-    if ( SSPCON2bits.ACKSTAT )return(36);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- Restart?? -----------------------
-    RestartI2C();                              // restart ????????????????????????
-    //if ( PIR2bits.BCLIF )return (37);        // ??????? 
-// ---------------- ??????Read Mode?? -----------------------------
-    if(WriteI2C(0b11010001))return(38);        // ??????? 
-    IdleI2C();// ??????
-    if ( SSPCON2bits.ACKSTAT )return(39);      // ACK?? 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-// ---------------- ????? -----------------------------
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[0] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[1] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-         
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[2] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-        
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[3] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-        
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[4] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[5] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-    //?????????
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-    
-    
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[6] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-   
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[7] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[8] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[9] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-    
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[10] = (unsigned int)  SSPBUF;       //???????
-    AckI2C(); 
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-    SSPCON2bits.RCEN = 1;                      // ??ON
-    while ( SSPCON2bits.RCEN );                // ??????
-    outbuf[11] = (unsigned int)  SSPBUF;       //???????
-    NotAckI2C();                               // ACK?????Not ACK)
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-           
-// ---------------- Stop condition?? --------------------------
-    StopI2C();                                 // ???????
-    PIR1bits.SSPIF = 0;                        // SSPIF???
-    
-    //while(!USBUSARTIsTxTrfReady()) CDCTxService();//??????????????????????      
-    putUSBUSART(outbuf, 28);
-    CDCTxService();
 }
 
 /***********************************************************
@@ -493,7 +239,7 @@ BOOL USER_USB_CALLBACK_EVENT_HANDLER(USB_EVENT event, void *pdata, WORD size)
             USBCBStdSetDscHandler();
             break;
         case EVENT_EP0_REQUEST:
-            USBCBCheckOtherReq();
+            USBCBCheckOtherReq();   
             break;
         case EVENT_BUS_ERROR:
             USBCBErrorHandler();
@@ -505,4 +251,4 @@ BOOL USER_USB_CALLBACK_EVENT_HANDLER(USB_EVENT event, void *pdata, WORD size)
             break;
     }      
     return TRUE; 
-}
+} 
