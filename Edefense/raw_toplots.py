2cdd1571bde390e3f35fa0ffea6be7871045cd76
kanopero <p-hat69@fuji.waseda.jp>
2019-02-05 17:41:11 +0900

    steps

--- Edefense/raw_toplots.py
+++ Edefense/raw_toplots.py
@@ -8,32 +8,53 @@ def main():
     mpl.rcParams['agg.path.chunksize'] = 100000
 
 
-    path = "//192.168.13.10/Public/hattori/" + \
-           "r4_ch13.csv"
+    path = "C:/Users/Tanii_Lab/Documents/"
+    read_name = "r4_ch13.csv"
+    out_name = "r4_ch13_out.csv"
     smp = 50
     fig = plt.figure(figsize=(20, 10))
 
-    df = pd.read_csv(path, delimiter=',')
-    df.columns = ["ch", "index", "X1", "Y1", "X2", "Y2"]
-    df.fillna(0)
-    array = df.as_matrix()
+    df0 = pd.read_csv(path+read_name, delimiter=',')
+    df0.columns = ["ch", "index", "X1", "Y1", "X2", "Y2"]
+    df0.fillna(0)
+    array = df0.as_matrix()
 
     # hex -> decimal
+    # v1 <-> y1, v2 <-> x2, v3 <-> y2, v4 <-> x1
     f = lambda x: int(x, 16)
-    x1 = np.array(list(map(f, array[:, 2])))
+    y1 = np.array(list(map(f, array[:, 2])))
     x2 = np.array(list(map(f, array[:, 3])))
-    y1 = np.array(list(map(f, array[:, 4])))
-    y2 = np.array(list(map(f, array[:, 5])))
-    print(x1)
-    print(x2)
-    print(y1)
-    print(y2)
+    y2 = np.array(list(map(f, array[:, 4])))
+    x1 = np.array(list(map(f, array[:, 5])))
+
+    x = ((x2+y1)-(x1-y2))/(x1+x2+y1+y2)
+    y = ((x2+y2)-(x1+y1))/(x1+x2+y1+y2)
+    time = np.arange(0, len(x)/smp, 1/smp)
+
+    # save
+    df1 = pd.DataFrame({'T [s]': time,
+                       'V1(Y1)': y1,
+                       'V2(X2)': x2,
+                       'V3(Y2)': y2,
+                       'V4(X1)': x1,
+                       'x': x,
+                       'y': y})
+    df1.to_csv(path+out_name)
 
     # plot
-    time = np.arange(0, len(x1)/smp, 1/smp)
-    print(time)
-    plt.plot(time, x1)
+    ax0 = fig.add_subplot(3, 1, 1)
+    ax1 = fig.add_subplot(3, 1, 2)
+    ax2 = fig.add_subplot(3, 1, 3)
+
+    ax0.plot(time, x)
+    ax1.plot(time, y)
+    ax2.plot(time, x-(abs(x[0])))
+    ax2.plot(time, y-(abs(y[0])))
+
+    fig.tight_layout()
+    plt.subplots_adjust(left=0.02, right=0.98, bottom=0.02, top=0.98)
     plt.show()
+
 
 if __name__ == '__main__':
     main()
