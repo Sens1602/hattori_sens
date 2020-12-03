73b031420201dd8b29327ce7872ebee5de7aebe3
kanopero <p-hat69@fuji.waseda.jp>
2020-12-03 13:09:06 +0900

    github

--- Edefense/ed2020/main.py
+++ Edefense/ed2020/main.py
@@ -13,23 +13,31 @@ def main():
     read_name_cal = "XYS200_DPSD2018_no2_121.csv"
     # experimental data
     read_name_meadata = "*ch*.csv"
-
-    smp = 50
-    fig = plt.figure(figsize=(20, 10))
-    ax = []
+    csvs = glob.glob(path+read_name_meadata)
+    print(csvs)
     # offset of relative story displacement
     # 耐震 No2, 3, 4, … 免振 No10, 11, 12, …
-    offsets_x = [10,20,30,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    offsets_y = [10, 20, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    offsets_x = [10, 20, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # mm
+    offsets_y = [10, 20, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # mm
+
+    smp = 50 # 50Hz
+    fig = plt.figure(figsize=(20, 10))
+    fig.suptitle("Horizontal axis: time [s], Vertical axis: Relative-story displacement [mm]")
+    ax = []
     num_plot = 8
+    """
+    ax[0]   ax[4]
+    ax[1]   ax[5]
+    ax[2]   ax[6]
+    ax[3]   ax[7]
+    """
     # 耐震
     for i in range(int(num_plot/2)):
-        ax.append(fig.add_subplot(4, 2, 2*(i+1)))
+        ax.append(fig.add_subplot(4, 2, 2*i+1))
     # 免振
     for i in range(int(num_plot/2)):
-        ax.append(fig.add_subplot(4, 2, 2*(i+2)))
+        ax.append(fig.add_subplot(4, 2, 2*i+2))
 
-    print(ax)
 
     ###### calibration ######
     df0 = pd.read_csv(path+read_name_cal, delimiter=',', skiprows=5)
@@ -56,9 +64,12 @@ def main():
     res2 = np.polyfit(y, ytrue, 1)
     YTRUE = np.poly1d(res2)(y)
 
+
     ###### Correction of experimental data using calibration data ######
+    """
     csvs = glob.glob(path+read_name_meadata)
-    #print(csvs)
+    print(csvs)
+    """
     f = lambda x: int(x, 16)
 
     for i in range(len(csvs)):
@@ -97,93 +108,22 @@ def main():
         filename, ext = os.path.splitext(os.path.basename(csvs[i]))
         #print(df2)
         df2.to_csv(path+filename+"_out.csv")
-        ax[i].plot(time, x_exp-offsets_x[i], color="red")
-        ax[i].plot(time, y_exp-offsets_y[i], color="blue")
+        ax[i].plot(time, 0.1*x_exp-offsets_x[i], color="red")
+        ax[i].plot(time, 0.1*y_exp-offsets_y[i], color="blue")
 
-    """
-    ###### plot ######
-    ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
-    ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=3)
-    ax3 = plt.subplot2grid((3, 3), (2, 0), colspan=2)
-    ax4 = plt.subplot2grid((3, 3), (2, 2))
-    ax1.scatter(x, xtrue)
-    ax1.plot(x, XTRUE)
-    ax2.scatter(y, ytrue)
-    ax2.plot(y, YTRUE)
+    ax[0].set_title("seismic isolated 1F")
+    ax[1].set_title("seismic isolated 2F")
+    ax[2].set_title("seismic isolated 3F")
+    ax[3].set_title("seismic isolated 4F")
+    ax[4].set_title("seismic force-resisting 1F")
+    ax[5].set_title("seismic force-resisting 2F")
+    ax[6].set_title("seismic force-resisting 3F")
+    ax[7].set_title("seismic force-resisting 4F")
 
-    #ax3.plot(time, -x_raw, color="blue", lw=0.5)
-    ax3.scatter(time, -x_raw, color="blue", s=5)
-    ax4.scatter(x, y)
-    ax5 = ax3.twinx()
-    #ax5.plot(time, x_exp, color="red", lw=0.5)
-    ax5.scatter(time, x_exp, color="red", s=5)
-    """
-    fig.tight_layout()
+    fig.tight_layout(rect=[0, 0, 1, 0.96])
     #plt.subplots_adjust(left=0.02, right=0.98, bottom=0.02, top=0.98)
     plt.show()
 
 
 if __name__ == '__main__':
     main()
-
-"""
-def main():
-    mpl.rcParams['agg.path.chunksize'] = 100000
-
-
-    #meadata_path = "//192.168.13.10/Public/sens/実験データ/2019_02_01/"
-    meadata_path = "C:/Users/Kouhei/Documents/edefense/20191204/"
-    calib_path = "C:/Users/Kouhei/Documents/edefense/calib_2020_before/"
-    read_name = "r1_ch11.csv"
-    out_name = "r3_ch09_out.csv"
-    smp = 50
-    fig = plt.figure(figsize=(20, 10))
-
-    df0 = pd.read_csv(meadata_path+read_name, delimiter=',')
-    df0.columns = ["ch", "index", "X1", "Y1", "X2", "Y2"]
-    df0.fillna(0)
-    array = df0.values
-    print(array)
-
-    # hex -> decimal
-    # v1 <-> y1, v2 <-> x2, v3 <-> y2, v4 <-> x1
-    f = lambda x: int(x, 16)
-    y1 = np.array(list(map(f, array[:, 2])))
-    x2 = np.array(list(map(f, array[:, 3])))
-    y2 = np.array(list(map(f, array[:, 4])))
-    x1 = np.array(list(map(f, array[:, 5])))
-
-    x = ((x2+y1)-(x1-y2))/(x1+x2+y1+y2)
-    y = ((x2+y2)-(x1+y1))/(x1+x2+y1+y2)
-    time = np.arange(0, len(x)/smp, 1/smp)
-
-    # save
-    df1 = pd.DataFrame({'T [s]': time,
-                       'V1(Y1)': y1,
-                       'V2(X2)': x2,
-                       'V3(Y2)': y2,
-                       'V4(X1)': x1,
-                       'x': x,
-                       'y': y})
-    df1.to_csv(meadata_path+out_name)
-
-    # plot
-    ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3, xlabel="time", ylabel="X Intensity /a.u.")
-    ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=3, xlabel="time", ylabel="Y Intensity /a.u.")
-    ax3 = plt.subplot2grid((3, 3), (2, 0), colspan=2, xlabel="time", ylabel="X and Y Intensity /a.u.")
-    ax4 = plt.subplot2grid((3, 3), (2, 2), xlabel="X Intensity /a.u.", ylabel="Y Intensity /a.u.")
-
-    ax1.plot(time, x)
-    ax2.plot(time, y)
-    ax3.plot(time, x-abs(x[0]))
-    ax3.plot(time, y-abs(y[0]))
-    ax4.plot(x-abs(x[0]), y-abs(y[0]))
-    plt.subplots_adjust(left=0.02, right=0.98, bottom=0.02, top=0.98)
-    fig.tight_layout()
-
-    plt.show()
-
-
-if __name__ == '__main__':
-    main()
-"""
